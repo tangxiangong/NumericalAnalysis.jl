3dea02b717e16c028e0ff61ee21b2d24c806b640
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-10-12 10:41:55 +0800

    Update NumericalAnalysis.jl

--- src/NumericalAnalysis.jl
+++ src/NumericalAnalysis.jl
@@ -1,9 +1,7 @@
 module NumericalAnalysis
 include("Fundamentals/Fundamentals.jl")
 include("NonLinearSolve/NonLinearSolve.jl")
-include("ExponentialIntegrators/ExponentialIntegrators.jl")
 export Fundamentals
 export NonLinearSolve
-export ExponentialIntegrators
 
 end # module NumericalAnalysis
